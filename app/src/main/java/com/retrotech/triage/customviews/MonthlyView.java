170f97ab4beb2592ebd495a19447928128bbef95
douglasTafeSA <douglas.hudson-walker@student.tafesa.edu.au>
2020-04-03 21:06:58 +1030

    Initial Setup

---
+++ app/src/main/java/com/retrotech/triage/customviews/MonthlyView.java
@@ -0,0 +1,12 @@
+package com.retrotech.triage.customviews;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class MonthlyView extends View {
+
+    public MonthlyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
